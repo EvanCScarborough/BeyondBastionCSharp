6ae513da781f004d089083ad2499707b8db302b5
evansca1215 <evan.c.scarborough@qtcm.com>
2023-06-29 16:37:55 -0400

    combat window

--- BeyondBastion/UI/CombatPlanWindow.Designer.cs
+++ BeyondBastion/UI/CombatPlanWindow.Designer.cs
@@ -28,10 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // CombatPlanWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(34)))), ((int)(((byte)(34)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "CombatPlanWindow";
+            this.ControlBox = false;
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "CombatPlanWindow";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
